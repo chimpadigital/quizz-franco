800a70b92a3d1f9114259149cd382888799ef805
chimpadigital <sdesigncba@gmail.com>
2019-10-31 11:54:53 -0300

    add kg y cm

--- php/mail.php
+++ php/mail.php
@@ -1,5 +1,5 @@
 <?php     
-	$to_email = 'sdesigncba@gmail.com';
+	$to_email = 'emailQueRecibiraLaInformacion@blablabla.com';
 	$subject = 'Franco-fit Quizz';
 	$message = "$_POST['name'] $_POST['lastName'] ha realizado el QUIZZ. E-mail: $_POST['email']. Peso: $_POST['weight'], Altura: $_POST['height'].";
 	$headers = 'From: noreply@franco-fit.com';

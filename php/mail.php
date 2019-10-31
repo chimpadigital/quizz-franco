ca758ed4ac2cb5151cf3c8082927b50398378367
chimpadigital <sdesigncba@gmail.com>
2019-10-31 11:04:55 -0300

    cambio mail envío

--- php/mail.php
+++ php/mail.php
@@ -1,5 +1,5 @@
 <?php     
-	$to_email = 'emailQueRecibiraLaInformacion@blablabla.com';
+	$to_email = 'sdesigncba@gmail.com';
 	$subject = 'Franco-fit Quizz';
 	$message = "$_POST['name'] $_POST['lastName'] ha realizado el QUIZZ. E-mail: $_POST['email']. Peso: $_POST['weight'], Altura: $_POST['height'].";
 	$headers = 'From: noreply@franco-fit.com';
